32caa747cf851a787235ba6bdd86ddcfc202927d
Thiago Scalone <thiago@scalone.com.br>
2014-11-18 00:57:33 -0200

    Check argv to execute exercise.

--- program.c
+++ program.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "ex1.h"
+#include "ex2.h"
+#include "ex3.h"
 
 int main(int argc, char **argv) {
-  ex1();
+  if (strcmp("ex1", (const char *)argv[1]) == 0)
+    ex1();
+  else if (strcmp("ex2", (const char *)argv[1]) == 0)
+    ex2();
+  else
+    ex3();
+
   return 0;
 }
