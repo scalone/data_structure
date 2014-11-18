512fc6a9b6f4bd5896608bae54b34eb050737255
Thiago Scalone <thiago@scalone.com.br>
2014-11-18 02:14:24 -0200

    Finish ex3.

--- ex3.c
+++ ex3.c
@@ -7,164 +7,105 @@
 
 typedef struct NODE {
   int value;
-  struct NODE *next;
   struct NODE *previous;
 } Node;
 
-typedef struct LIST {
-  Node *last;
-  Node *first;
-  int top;
-} List;
+typedef struct STACK {
+  Node *top;
+  int size;
+} Stack;
 
-void EX3_display(List *list)
+void EX3_display(Stack *stack)
 {
   Node *p;
   int index;
 
-  for (index=0,p=list->first->next; p != NULL;index++,p=p->next) {
+  for (index=stack->size,p=stack->top; p != NULL;index--,p=p->previous) {
     printf("%d - %d\n", index, p->value);
   }
 }
 
-Node *EX3_find(List *list, int x)
+
+void EX3_push(Stack *stack, int value)
 {
   Node *p;
-  int index;
-
-  for (index=0,p=list->first; p != NULL;index++,p=p->next) {
-    if(index==x) return p;
-  }
-
-	return NULL;
-}
-
-void EX3_insert(List *list, int value, int position)
-{
-  Node *p;
-  Node *old;
-  Node *aux;
 
   if ((p = malloc(sizeof(Node))) == NULL)
     printf("Memory allocation error");
   else {
+    p->previous = stack->top;
     p->value = value;
-    old = (position == -1) ? EX3_find(list, list->top) : EX3_find(list, position);
-    aux = old->next;
-
-    p->next = aux;
-    aux->previous = p;
-    old->next = p;
-    p->previous = old;
-
-    list->top++;
-    if (aux == NULL) list->last = p;
+    stack->top = p;
+    stack->size++;
   }
 }
 
-void EX3_change(Node *node, int new)
+bool EX3_isEmpty(Stack *stack)
 {
-  if (node != NULL) node->value = new;
+  return (stack->size == 0);
 }
 
-bool EX3_isEmpty(List *list)
+int EX3_pop(Stack *stack)
 {
-  return (list->first == list->last);
-}
-
-bool EX3_delete(List *list, Node *p)
-{
-  Node *pdel;
+  Node *p;
   int value;
 
-  if (EX3_isEmpty(list) || p == NULL || (pdel = p->next) == NULL) return false;
+  if (EX3_isEmpty(stack)) {
+    printf("Stack empty");
+    return 0;
+  }
 
-  p->next = p->next->next;
-  p->previous = p->previous->previous;
-  value   = pdel->value;
-
-  if (p->next == NULL) list->last = p;
-  if (p->previous == NULL) list->first = p;
-  list->top--;
-  free(pdel);
+  p = stack->top;
+  stack->top = stack->top->previous;
+  stack->size--;
+  value = p->value;
+  free(p);
 
   return value;
 }
 
-void EX3_initialize(List *list)
+void EX3_initialize(Stack *stack)
 {
-	if ((list->first = malloc(sizeof(Node))) == NULL)
-		printf("Memory allocation error");
-	else {
-    list->last = list->first;
-    list->last->value = 0;
-    list->last->next = NULL;
-    list->last->previous = NULL;
-    list->top = 0;
-	}
+  stack->top = NULL;
+  stack->size = 0;
 }
 
-void EX3_destroy(List *list)
+void EX3_destroy(Stack *stack)
 {
-  while(! EX3_isEmpty(list)) EX3_delete(list, list->first);
+  while(! EX3_isEmpty(stack)) EX3_pop(stack);
 }
 
 int ex3(void)
 {
-  List list;
-  int loop, value, valueNew;
-  char *options="\n 1 - Display \n 2 - insert \n 3 - insert in position \n 4 - Change \n 5 - Find \n 6 - Remove \n 7 - Destroy \n 0 - Exit\0";
+  Stack stack;
+  int loop, value;
+  char *options="\n 1 - Display \n 2 - Push \n 3 - Pop \n 4 - Destroy \n 0 - Exit\0";
 
   loop = 1;
 
-  printf("\n Exercise 2\n");
+  printf("\n Exercise 3\n");
 
-  EX3_initialize(&list);
+  EX3_initialize(&stack);
 
   while (loop) {
     switch(menu(options)) {
       case 1:
-        EX3_display(&list);
+        EX3_display(&stack);
         break;
 
       case 2:
         printf("Number to insert: ");
         scanf("%d", &value);
-        EX3_insert(&list, value, -1);
+        EX3_push(&stack, value);
         break;
 
       case 3:
-        printf("Number to insert: ");
-        scanf("%d", &value);
-        printf("Index to insert: ");
-        scanf("%d", &valueNew);
-        EX3_insert(&list, value, valueNew);
+        value = EX3_pop(&stack);
+        printf("Pop value: %d", value);
         break;
 
       case 4:
-        printf("Element to Change: ");
-        scanf("%d", &value);
-        printf("New number: ");
-        scanf("%d", &valueNew);
-        EX3_change(EX3_find(&list, value), valueNew);
-        printf("Element Changed");
-        break;
-
-      case 5:
-        printf("Element index: ");
-        scanf("%d", &value);
-        printf("Element Value is: %d ", (EX3_find(&list, value))->value);
-        break;
-
-      case 6:
-        printf("Element index: ");
-        scanf("%d", &value);
-        EX3_delete(&list, EX3_find(&list, value));
-        printf("Element Removed");
-        break;
-
-      case 7:
-        EX3_destroy(&list);
+        EX3_destroy(&stack);
         printf("Destroyed");
         break;
 
